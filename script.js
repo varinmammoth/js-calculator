8c8653b9dc2568019109094789132aabd15503e3
varinmammoth <varinmammoth@gmail.com>
2021-09-02 16:17:54 +0700

    Finished bvasic UI

---
+++ script.js
@@ -0,0 +1,33 @@
+//basic math functions
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a,b,operator) {
+    switch (operator) {
+        case 'add':
+            return add(a,b);
+            break;
+        case 'subtract':
+            return subtract(a,b);
+            break;
+        case 'multiply':
+            return multiply(a,b);
+            break;
+        case 'divide':
+            return divide(a,b);
+            break;
+    }
+}
