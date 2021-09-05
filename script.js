9e5300d93f5bf9d0ea0bd33240affc41b71dbf78
varinmammoth <varinmammoth@gmail.com>
2021-09-05 15:55:44 +0700

    Started algorithm

--- script.js
+++ script.js
@@ -31,3 +31,47 @@ function operate(a,b,operator) {
             break;
     }
 }
+
+let a = 0;
+let b = 0;
+let operator = 'add';
+let userInput = '';
+
+function clear() {
+    a = 0;
+    b = 0;
+    operator = 'add';
+    userInput = '';
+    document.querySelector('.userInputDisp').textContent = userInput;
+}
+
+function equal() {
+    a = operate(a, b, operator);
+    document.querySelector('.userInputDisp').textContent = a;
+}
+
+function evaluateUserInput(userInput) {
+    
+}
+
+let btn = document.querySelectorAll('btn');
+btn = Array.from(btn);
+btn.forEach( (button) => {
+    button.addEventListener( 'click', () => {
+        let content = button.textContent;
+        if (content=='+' || content=='-' || content=='x' || content=='/') {
+            userInput += content;
+        }
+        else if (content=='clr') {
+            clear();
+        }
+        else if (content =='=') {
+            operate(a, b, operator);
+        }
+        else {
+            userInput += content;
+            document.querySelector('.userInputDisp').textContent = userInput;
+        }
+    })
+})
+
